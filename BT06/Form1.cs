001aa176ad7fa31d864c79b41e8b52b50c40a2a7
Tâm Phan <phantam04012003@gmail.com>
2023-03-30 16:42:35 +0700

    Add project files.

---
+++ BT06/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace BT06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadCBBLop();
+            LoadData();
+            LoadCBBSort();
+        }
+        
+        private void LoadCBBLop()
+        {
+            using(TamDB db = new TamDB())
+            {
+                var lopSh = db.SVs.Select(sv => sv.Class).ToList();
+                lopSh = lopSh.Distinct().ToList();
+                lopSh.Insert(0, "All");
+                lopSh.Sort();
+                cbLopSH.DataSource = lopSh;
+            }
+        }
+
+        private void LoadCBBSort()
+        {
+            cbSort.DataSource = new List<string>() { "ID", "Name", "GPA" };
+        }
+        public void LoadData()
+        {
+            using(var db = new TamDB()) 
+            {
+                var listSv = db.SVs.Select(sv => sv).ToList();
+                gvTable.DataSource = listSv;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string name = txtSearch.Text;
+            string lop = cbLopSH.SelectedItem.ToString();
+            lop = lop == "All" ? "" : lop;
+
+            using (var db = new TamDB())
+            {
+                var listSv = db.SVs.Where(sv => sv.Name.Contains(name) && sv.Class.Contains(lop)).ToList();
+                gvTable.DataSource = listSv;
+            }
+           
+        }
+
+        private void cbLopSH_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string lop = cbLopSH.SelectedItem.ToString();
+            lop = lop == "All" ? "" : lop;
+
+            using (var db = new TamDB())
+            {
+                var listSv = db.SVs.Where(sv => sv.Class.Contains(lop)).ToList();
+                gvTable.DataSource = listSv;
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Bạn có muốn xoá không", "Xác nhận xoá", MessageBoxButtons.OKCancel);
+            if (res == DialogResult.OK)
+            {
+                List<string> listId = new List<string>();
+                foreach (DataGridViewRow row in gvTable.SelectedRows)
+                {
+                    listId.Add(row.Cells["ID"].Value.ToString());
+                }
+
+                using (var db = new TamDB())
+                {
+                    var listSv = db.SVs.Where(sv => listId.Contains(sv.ID));
+                    db.SVs.RemoveRange(listSv);
+                    db.SaveChanges();
+                    btnSearch_Click(sender, e);
+                }
+            }
+
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            string option = cbSort.SelectedItem.ToString();
+            using (var db = new TamDB())
+            {
+                List<SV> listSv = null;
+                if (option == "ID")
+                {
+                    listSv = db.SVs.Select(sv => sv).ToList().OrderBy(sv=>sv.ID).ToList();
+                }
+                else if (option == "Name") {
+                    listSv = db.SVs.Select(sv => sv).ToList().OrderBy(sv => sv.Name).ToList();
+                }
+                else
+                {
+                    listSv = db.SVs.Select(sv => sv).ToList().OrderBy(sv => sv.GPA).ToList();
+                }
+                gvTable.DataSource = listSv;
+                
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.myDel += LoadData;
+            form2.ShowDialog();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (gvTable.SelectedRows.Count == 1)
+            {
+                string mssv = gvTable.SelectedRows[0].Cells[0].Value.ToString();
+                Form2 f2 = new Form2(mssv);
+                f2.myDel += LoadData;
+                f2.ShowDialog();
+
+            }
+        }
+
+       
+    }
+}
---
+++ BT06/Form2.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BT06
+{
+    public partial class Form2 : Form
+    {
+        public delegate void LoadForm1();
+        public LoadForm1 myDel;
+        private string _id;
+
+        // add
+        public Form2()
+        {
+
+            InitializeComponent();
+            InitAdd();
+        }
+
+        // edit
+        public Form2(string id)
+        {
+            _id = id;
+            InitializeComponent();
+            InitEdit();
+        }
+
+        public void LoadCBLopSH()
+        {
+            using (TamDB db = new TamDB())
+            {
+                var lopSh = db.SVs.Select(sv => sv.Class).ToList();
+                lopSh = lopSh.Distinct().ToList();
+                
+                lopSh.Sort();
+                cbLopSh.DataSource = lopSh;
+            }
+        }
+
+        public void InitAdd()
+        {
+            LoadCBLopSH();
+            btnOk.Click += new System.EventHandler(this.btnOk_ClickAdd);
+            txtMSSV.Enabled = true;
+        }
+
+        public void InitEdit()
+        {
+            LoadCBLopSH();
+            btnOk.Click += new System.EventHandler(this.btnOk_ClickEdit);
+
+            using (TamDB db = new TamDB())
+            {
+                var sv = db.SVs.Where(s => s.ID == _id).SingleOrDefault();
+
+                // load sv
+                txtMSSV.Text = sv.ID;
+                txtName.Text = sv.Name;
+                cbLopSh.SelectedIndex = cbLopSh.Items.IndexOf(sv.Class);
+                dtNs.Text = sv.Dob.ToString();
+                if ((bool)sv.Gender)
+                {
+                    rbtnMale.Checked = true;
+                }
+                else
+                {
+                    rbtnFemale.Checked = true;
+                }
+                txtDTB.Text = sv.GPA.ToString();
+                checkAnh.Checked = (bool)(sv.Picture);
+                checkHocBa.Checked = (bool)(sv.School_report);
+                checkCCCD.Checked = (bool)sv.CCCD;
+
+            }
+        }
+        private void btnOk_ClickEdit(object sender, System.EventArgs e)
+        {
+            var ID = txtMSSV.Text;
+            var Name = txtName.Text;
+            var Dob = Convert.ToDateTime(dtNs.Value.ToShortDateString());
+            float GPA;
+            float.TryParse(txtDTB.Text, out GPA);
+
+            bool checkOK = true;
+            if (Name != "")
+            {
+                using (TamDB db = new TamDB())
+                {
+                    var sV = db.SVs.Where(s => s.ID == _id).SingleOrDefault();
+                    sV.ID = ID;
+                    sV.Name = Name;
+                    sV.Class = cbLopSh.SelectedItem.ToString();
+                    sV.Dob = Dob;
+                    sV.GPA = GPA;
+                    sV.Gender = rbtnMale.Checked;
+                    sV.Picture = checkAnh.Checked;
+                    sV.School_report = checkHocBa.Checked;
+                    sV.CCCD = checkCCCD.Checked;
+
+                    db.SaveChanges();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Thieu thong tin");
+                checkOK = false;
+            }
+            if (checkOK)
+            {
+                myDel.Invoke();
+                this.Dispose();
+            }
+        }
+        private void btnOk_ClickAdd(object sender, EventArgs e)
+        {
+            var ID = txtMSSV.Text;
+            var Name = txtName.Text;
+            var Dob = Convert.ToDateTime(dtNs.Value.ToShortDateString());
+            float GPA;
+            float.TryParse(txtDTB.Text, out GPA);
+
+            // ktra Id co ton tai hay khong
+           
+            bool checkOK = true;
+
+            using (TamDB db = new TamDB())
+            {
+                var sV = db.SVs.Where(s => s.ID == ID).SingleOrDefault();
+
+                if (sV==null)
+                {
+                    if (ID != "" && ID.Length <= 8 && Name != "")
+                    {
+                        sV = new SV();
+                        sV.ID = ID;
+                        sV.Name = Name;
+                        sV.Class = cbLopSh.SelectedItem.ToString();
+                        sV.Dob = Dob;
+                        sV.GPA = GPA;
+                        sV.Gender = rbtnMale.Checked;
+                        sV.Picture = checkAnh.Checked;
+                        sV.School_report = checkHocBa.Checked;
+                        sV.CCCD = checkCCCD.Checked;
+
+                        db.SVs.Add(sV);
+
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Thieu thong tin");
+                        checkOK = false;
+                    }
+                }
+                else
+                {
+                    checkOK = false;
+                    MessageBox.Show("MSSV da ton tai");
+                }
+                if (checkOK)
+                {
+                    myDel.Invoke();
+                    this.Dispose();
+
+                }
+            }     
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Dispose(); 
+        }
+    }
+}
